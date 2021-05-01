cf07d9c531d57dc0a743563b0467e8ae2d6b214a
Oscar Silva <46907491+Oscar-Silva@users.noreply.github.com>
2021-05-01 12:24:59 -0300

    Import fonts to application

---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,15 @@
+const fonts = {
+  marvel: require("./MarvelRegular.ttf"),
+  axiforma: require("./AxiformaRegular.otf"),
+  "axiforma-bold": require("./AxiformaBold.otf"),
+  "axiforma-black": require("./AxiformaBlack.otf"),
+  "axiforma-book": require("./AxiformaBook.otf"),
+  "axiforma-extra-bold": require("./AxiformaExtraBold.otf"),
+  "axiforma-heavy": require("./AxiformaHeavy.otf"),
+  "axiforma-light": require("./AxiformaLight.otf"),
+  "axiforma-medium": require("./AxiformaMedium.otf"),
+  "axiforma-semibold": require("./AxiformaSemiBold.otf"),
+  "axiforma-thin": require("./AxiformaThin.otf"),
+};
+
+export default fonts;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { View } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import theme from "./commom/theme";
 import MainNavigator from "./navigations";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+import fonts from "./assets/fonts";
 
 const MainApp = () => {
+  const [loadedFonts, error] = useFonts({
+    ...fonts,
+  });
+
+  if (!loadedFonts) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
