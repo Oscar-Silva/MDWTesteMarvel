463bd0db2aff65f7d50bb4a63d4feb65e793abbf
Oscar Silva <46907491+Oscar-Silva@users.noreply.github.com>
2021-05-02 14:05:47 -0300

    Add component MenuModal to use with a SideBarPin

---
+++ src/commom/components/MenuModal/index.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Image, View } from "react-native";
+
+import {
+  ContainerModal,
+  Container,
+  ProfileContainer,
+  Wrapper,
+  ProfilePictureContainer,
+  ProfileMenuContainer,
+  ProfileMenuItem,
+  ProfileMenuItemText,
+} from "./styles";
+import ImgProfilePicture from "../../../assets/images/TopBar/profilePicture.png";
+import { useNavigation } from "@react-navigation/core";
+
+interface ProfileModalProps {
+  visible: boolean;
+  onRequestClose(): void;
+}
+
+const MenuModal = ({
+  visible = false,
+  onRequestClose = () => {},
+}: ProfileModalProps) => {
+  const navigation = useNavigation();
+
+  const onExitPress = () => {
+    onRequestClose();
+    navigation.navigate("login");
+  };
+
+  return (
+    <ContainerModal
+      visible={visible}
+      transparent
+      onRequestClose={onRequestClose}
+    >
+      <Container>
+        <Wrapper />
+        <ProfileContainer>
+          <ProfilePictureContainer>
+            <Image
+              source={ImgProfilePicture}
+              width={55}
+              height={55}
+              style={{ height: 55, width: 55, borderRadius: 100 }}
+            />
+          </ProfilePictureContainer>
+
+          <ProfileMenuContainer>
+            <ProfileMenuItem>
+              <ProfileMenuItemText>Perfil</ProfileMenuItemText>
+            </ProfileMenuItem>
+            <ProfileMenuItem>
+              <ProfileMenuItemText>Configurações</ProfileMenuItemText>
+            </ProfileMenuItem>
+            <ProfileMenuItem>
+              <ProfileMenuItemText>Visite nosso site</ProfileMenuItemText>
+            </ProfileMenuItem>
+            <ProfileMenuItem onPress={onExitPress}>
+              <ProfileMenuItemText>Sair</ProfileMenuItemText>
+            </ProfileMenuItem>
+          </ProfileMenuContainer>
+        </ProfileContainer>
+      </Container>
+    </ContainerModal>
+  );
+};
+
+export default MenuModal;
---
+++ src/commom/components/MenuModal/styles.ts
@@ -0,0 +1,64 @@
+import {
+  ImageProps,
+  ModalProps,
+  TextProps,
+  TouchableOpacityProps,
+  ViewProps,
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import styled from "styled-components/native";
+
+export const ContainerModal = styled.Modal<ModalProps>`
+  display: flex;
+  flex: 1;
+`;
+
+export const Container = styled.View<ViewProps>`
+  display: flex;
+  flex: 1;
+  background-color: rgba(0, 0, 0, 0.8);
+`;
+export const Wrapper = styled.View<ViewProps>`
+  display: flex;
+  height: 20px;
+  background-color: ${(props) => props.theme.colors.primary};
+  z-index: 1;
+`;
+
+export const ProfileContainer = styled.View<ViewProps>`
+  display: flex;
+  height: 280px;
+  width: 228px;
+  margin-left: -35px;
+  padding-left: 55px;
+  padding-top: 25px;
+  box-shadow: 1px 3px 6px rgba(225, 0, 0, 1);
+  background-color: ${(props) => props.theme.colors.primary};
+  border-radius: 36px;
+  border-top-right-radius: 0px;
+`;
+
+export const ProfilePictureContainer = styled.View<ViewProps>`
+  width: 55px;
+  height: 55px;
+  box-shadow: 3px 3px 3px rgba(225, 0, 0, 1);
+`;
+
+export const ProfileMenuContainer = styled.View<ViewProps>`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  margin-top: -35px;
+`;
+
+export const ProfileMenuItem = styled.TouchableOpacity<TouchableOpacityProps>`
+  display: flex;
+  height: 18px;
+  margin-top: 15px;
+`;
+
+export const ProfileMenuItemText = styled.Text<TextProps>`
+  font-size: 17px;
+  font-family: axiforma;
+  color: ${(props) => props.theme.colors.secondary};
+`;
