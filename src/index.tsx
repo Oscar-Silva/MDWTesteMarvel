42314bde28b7562b4161086d8fb8bdcb59fe4626
Oscar Silva <46907491+Oscar-Silva@users.noreply.github.com>
2021-05-02 02:48:28 -0300

    Configure the Redux Provider Store in MainApp

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { View } from "react-native";
+
 import { ThemeProvider } from "styled-components/native";
 import theme from "./commom/theme";
-import MainNavigator from "./navigations";
+import Navigator from "./navigations";
 import { useFonts } from "expo-font";
 import AppLoading from "expo-app-loading";
 import fonts from "./assets/fonts";
+import { Provider } from "react-redux";
+import store from "./redux";
 
 const MainApp = () => {
   const [loadedFonts, error] = useFonts({
@@ -18,11 +20,13 @@ const MainApp = () => {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <MainNavigator />
-      </NavigationContainer>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <Navigator />
+        </NavigationContainer>
+      </ThemeProvider>
+    </Provider>
   );
 };
 
