fde1a80a99ea2ae0682eda2ab15e34c756e64e1e
Oscar Silva <46907491+Oscar-Silva@users.noreply.github.com>
2021-05-03 04:44:47 -0300

    Add requestCharactersSeries
    to get series of a specific character

---
+++ src/services/api/characters/requestCharactersSeries.ts
@@ -0,0 +1,16 @@
+import api, { getAuthURL } from "..";
+import SeriesDataWrapper, { Series } from "../../../models/series";
+
+export default async function requestCharactersSeries(
+  characterID: number,
+  limit: number = 10
+): Promise<Series[] | undefined> {
+  console.log(characterID);
+  const result: SeriesDataWrapper = (
+    await api.get(getAuthURL(`/characters/${characterID}/series`), {
+      params: { limit, orderBy: "-modified" },
+    })
+  ).data;
+
+  return result.data?.results;
+}
