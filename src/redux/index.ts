2406dbd62aedd46e05aa153f7f4046a4d18d8ff9
Oscar Silva <46907491+Oscar-Silva@users.noreply.github.com>
2021-05-03 18:46:19 -0300

    Update the store to add series reducer

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -2,19 +2,22 @@ import { all, fork } from "redux-saga/effects";
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import charactersReducer from "./characters";
+import seriesReducer from "./series";
 import charactersSaga from "./characters/sagas";
 import sideBarPinReducer from "./sideBarPin";
+import seriesSaga from "./series/sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
 function* rootSaga() {
-  yield all([fork(charactersSaga)]);
+  yield all([fork(charactersSaga), fork(seriesSaga)]);
 }
 
 const store = configureStore({
   reducer: {
     charactersReducer,
     sideBarPinReducer,
+    seriesReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(sagaMiddleware),
