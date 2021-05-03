70da0961b4ddd5fec3ba216ac33b1cecf1d6c16f
Oscar Silva <46907491+Oscar-Silva@users.noreply.github.com>
2021-05-03 02:48:15 -0300

    Update login add onLoginPress to open navigator main

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,24 +1,35 @@
+import { useNavigation } from "@react-navigation/core";
 import { LinearGradient } from "expo-linear-gradient";
 import React, { useState } from "react";
 import { View } from "react-native";
 import Animated from "react-native-reanimated";
 import MarvelTitle from "../../commom/components/MarvelTitle";
 import Splash from "../../commom/components/Splash";
+
 import BackImage from "./BackImage";
 import Form from "./Form";
 import MarvelHeader from "./MarvelHeader";
 import { Container } from "./styles";
 
 const Login = () => {
+  const navigation = useNavigation();
   const [splashRuning, setSplashRuning] = useState(true);
   if (splashRuning) {
     return <Splash onDoneSplash={() => setSplashRuning(false)} />;
   }
 
+  const onLoginPress = async (
+    email: string,
+    password: string,
+    rememberUser: boolean
+  ) => {
+    navigation.navigate("main");
+  };
+
   return (
     <Container>
       <MarvelHeader />
-      <Form />
+      <Form onLoginPress={onLoginPress} />
       <BackImage />
     </Container>
   );
