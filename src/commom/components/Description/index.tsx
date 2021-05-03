2ca76a56377e7e39c78d97164cb33a2cc88b15dc
Oscar Silva <46907491+Oscar-Silva@users.noreply.github.com>
2021-05-03 03:13:44 -0300

    Add description commom component

--- src/commom/components/Description/index.tsx
+++ src/commom/components/Description/index.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import { View } from "react-native";
 
-// import { Container } from './styles';
+import {
+  Container,
+  DescriptionText,
+  ShowMoreButton,
+  ShowMoreButtonText,
+} from "./styles";
 
-const Description = () => {
-  return <View />;
+interface DescriptionProps {
+  description: string;
+  onShowMorePress?(): void;
+}
+
+const Description = ({
+  description,
+  onShowMorePress = () => {},
+}: DescriptionProps) => {
+  console.log(description);
+  return (
+    <Container>
+      <DescriptionText>{description}</DescriptionText>
+      <ShowMoreButton onPress={onShowMorePress}>
+        <ShowMoreButtonText>ver mais</ShowMoreButtonText>
+      </ShowMoreButton>
+    </Container>
+  );
 };
 
 export default Description;
--- src/commom/components/Description/styles.ts
+++ src/commom/components/Description/styles.ts
@@ -1,3 +1,23 @@
+import { PressableProps, TextProps, TouchableOpacityProps } from "react-native";
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  max-height: 92px;
+  padding: 10px;
+  margin-top: 10px;
+`;
+
+export const DescriptionText = styled.Text<TextProps>`
+  font-size: 14px;
+  color: ${(props) => props.theme.colors.terciary};
+  font-family: axiforma-thin;
+`;
+
+export const ShowMoreButton = styled.TouchableOpacity<TouchableOpacityProps>`
+  padding-top: 10px;
+`;
+export const ShowMoreButtonText = styled.Text<TextProps>`
+  font-size: 10px;
+  font-family: axiforma-book;
+  color: ${(props) => props.theme.colors.secondary};
+`;
